58bc4e54def9c20c7e26bcd279c492764da24ffb
Rebe <r.lopez@itecriocuarto.org.ar>
2025-08-14 17:32:39 -0300

    Update urls.py

--- airlines/airline/urls.py
+++ airlines/airline/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('apps.accounts.urls')),            
     path('flights/', include('apps.flights.urls')),     
     path('passengers/', include('apps.passengers.urls')), 
