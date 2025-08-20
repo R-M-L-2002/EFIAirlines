2615e097dbe3af7ccb50fd8ea33053a7377fd01d
Rebe <r.lopez@itecriocuarto.org.ar>
2025-08-19 22:37:33 -0300

    Update urls.py

--- airlines/apps/reservations/urls.py
+++ airlines/apps/reservations/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('confirm/<str:reservation_code>/', views.confirm_reservation, name='confirm'),
     path('cancel/<str:reservation_code>/', views.cancel_reservation, name='cancel'),
     path('mine/', views.my_reservations, name='my_reservations'),  # ← aquí antes de detail
-    path('<str:reservation_code>/', views.reservation_detail, name='detail'),
+    path('<str:reservation_code>/', views.detail, name='detail'),
+    
 ]
